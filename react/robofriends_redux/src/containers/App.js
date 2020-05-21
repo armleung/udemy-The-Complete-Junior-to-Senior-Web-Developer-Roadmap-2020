efd979686979135288287e037850ffefb873410a
leoleung2000 <leoleung2000@yahoo.com.hk>
2020-05-21 13:51:41 +0800

    add codes for API calls

--- react/robofriends_redux/src/containers/App.js
+++ react/robofriends_redux/src/containers/App.js
@@ -5,17 +5,21 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = (state) => {
   return {
-    searchfield : state.searchField
+    searchfield : state.searchRobots.searchField,
+    robots      : state.requestRobots.robots,
+    isPending   : state.requestRobots.isPending,
+    error       : state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange : (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobot : () => dispatch(requestRobots())
   }
 }
 
@@ -25,23 +29,17 @@ const mapDispatchToProps = (dispatch) => {
 // }
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: []
-      //searchfield: ''
-    }
-  }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => {this.setState({ robots: users})});
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => response.json())
+    //   .then(users => {this.setState({ robots: users})});
+    this.props.onRequestRobot();
   }
 
   render() {
-    const { robots } = this.state;
-    const { searchfield, onSearchChange } = this.props;
+    //const { robots } = this.state;
+    const { searchfield, onSearchChange, robots } = this.props;
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
--- react/robofriends_redux/src/index.js
+++ react/robofriends_redux/src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createLogger } from 'redux-logger';
-import { createStore , applyMiddleware } from 'redux';
+import { createStore , applyMiddleware, combineReducers } from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots , requestRobots } from './reducers';
+import thunkMiddleware from 'redux-thunk';
 import 'tachyons';
 
 const logger = createLogger()
-const store = createStore(searchRobots,applyMiddleware(logger))
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = 
+    createStore(rootReducer,applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
     <Provider store={store}>
